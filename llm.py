76d5062e82ed2f3d2f7c9a36ed4c6c40d4771e11
진경원 사원 <kw.jhin@lgcns.com>
2025-04-15 17:13:20 +0900

    refactoring

--- llm.py
+++ llm.py
@@ -5,23 +5,23 @@ from langchain.chains import RetrievalQA
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-def get_ai_message(user_message):
 
+def get_llm():
+    llm = ChatUpstage()
+    return llm
+
+
+def get_retriever():
     embeddings = UpstageEmbeddings(model="embedding-query")
     index_name = 'tax-markdown-index'
     database = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embeddings)
-
-    llm = ChatUpstage()
-
-    rag_prompt = hub.pull("rlm/rag-prompt")
-
     retriever = database.as_retriever(search_kwargs={'k': 4})
+    return retriever
 
-    qa_chain = RetrievalQA.from_llm(
-        llm, retriever=database.as_retriever(), prompt=rag_prompt
-    )
 
+def get_dictionary_chain():
     dictionary = ["사람을 나타내는 표현 -> 거주자"]
+    llm = get_llm()
     prompt = ChatPromptTemplate.from_template(f"""
         사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해주세요.
         만약 변경할 필요가 없다고 판단된다면, 사용자의 질문을 변경하지 않아도 됩니다.
@@ -31,7 +31,20 @@ def get_ai_message(user_message):
         질문: {{question}}
     """)
     dictionary_chain = prompt | llm | StrOutputParser()
+    return dictionary_chain
 
+def get_qa_chain():
+    llm = get_llm()
+    retriever = get_retriever()
+    rag_prompt = hub.pull("rlm/rag-prompt")
+    qa_chain = RetrievalQA.from_llm(llm, retriever=retriever, prompt=rag_prompt)
+    return qa_chain
+
+
+def get_ai_message(user_message):
+    dictionary_chain = get_dictionary_chain()
+    qa_chain = get_qa_chain()
     tax_chain = {"query": dictionary_chain} | qa_chain
     ai_message = tax_chain.invoke({"question": user_message})
+
     return ai_message["result"]
